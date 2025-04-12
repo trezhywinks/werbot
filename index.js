5426d40e9c3cd06770d8332a1de33d695a865ac3
trezhywinks <dreqxyonion@gmail.com>
2025-04-12 01:11:00 +0000

    werbot

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const figlet = require('figlet');
 const banner = "Botsapp";
+const makeWASocket = require('@whiskeysockets/baileys').default;
+const { useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const QRCode = require('qrcode-terminal');
 
-function cons(){
-console.log("  [-] New Message.");
-}
+const prefix = "."; // Definição do prefixo
 
 function ban(){
 figlet.text(banner, (err, data) => {
@@ -12,8 +13,69 @@ console.log('[!] Erro => ', err);
 return;
 }
 console.log(data);
-cons()
 })
-}
+};
 
-ban()
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState('sessao');
+
+    const whmer = makeWASocket({
+        auth: state,
+        printQRInTerminal: true,
+    });
+
+    whmer.ev.on('creds.update', saveCreds);
+
+    whmer.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect, qr } = update;
+
+        if (qr) {
+            QRCode.generate(qr, { small: true });
+        }
+
+        if (connection === 'close') {
+            const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== 401;
+            if (shouldReconnect) {
+                startBot();
+            }
+        } else if (connection === 'open') {
+            console.log('Bot conectado!');
+        }
+    });
+
+     whmer.ev.on('messages.upsert', async (m) => {
+        const msg = m.messages[0];
+        if (!msg.message || msg.key.fromMe) return;
+
+        const text = msg.message.conversation || msg.message.extendedTextMessage?.text || '';
+        const sender = msg.key.remoteJid;
+
+        //   view
+        await whmer.readMessages([msg.key]);
+       console.log(msg);
+        //  cases
+        if (text.startsWith(prefix)) {
+            const command = text.slice(1).trim().split(" ")[0];
+            switch (command) {
+                case "fingmais":
+                    await whmer.sendMessage(sender, { text: "Pong!" });
+                    break;
+
+                case "status":
+                    await sendStatus(whmer, sender);
+                    break;
+
+                default:
+                   // await whmer.sendMessage(sender, { text: "Comando não reconhecido." });
+            }
+        }
+    });
+
+    // view status 
+    async function sendStatus(sock, sender) {
+        const status = await sock.fetchStatus(sender);
+        await sock.sendMessage(sender, { text: `Status atual: ${status.status || "Nenhum status disponível"}` });
+    }
+}
+ban();
+startBot();
